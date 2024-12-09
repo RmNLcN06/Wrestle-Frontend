97a0b30074d0d8d27fbbb2dc33f1796e5eabd083
RmNLcN06 <romain.luciano@orange.fr>
2024-12-09 23:16:04 +0100

    Add continent routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ContinentListComponent } from './continent/continent-list/continent-list.component';
+import { ContinentFormComponent } from './continent/continent-form/continent-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: '',
+        component: ContinentListComponent,
+    },
+    { 
+        path: 'add',
+        component: ContinentFormComponent,
+    },
+    { 
+        path: 'edit/:id',
+        component: ContinentFormComponent,
+    },
+];
