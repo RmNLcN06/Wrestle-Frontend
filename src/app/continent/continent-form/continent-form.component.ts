ab463e648777204c96d3284302bd78cbf4c1c353
RmNLcN06 <romain.luciano@orange.fr>
2024-12-09 22:23:29 +0100

    Add continent-form methods

--- src/app/continent/continent-form/continent-form.component.ts
+++ src/app/continent/continent-form/continent-form.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ContinentService } from '../continent.service';
+import { Continent } from '../continent';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-continent-form',
@@ -7,5 +10,43 @@ import { Component } from '@angular/core';
   styleUrl: './continent-form.component.scss'
 })
 export class ContinentFormComponent {
+  continent: Continent = { id: 0, name: ''};
+  isEditMode: boolean = false;
 
+  constructor(private continentService: ContinentService, private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if(id) {
+      this.isEditMode = true;
+      this.continentService.getContinentById(+id).subscribe({
+        next: (data) => (this.continent = data),
+        error: (err) => console.error('Error fetching continent: ', err),
+      });
+    }
+  }
+
+  saveContinent(): void {
+    if(this.isEditMode) {
+      this.continentService.updateContinent(this.continent.id!, this.continent).subscribe({
+        next: () => {
+          alert('Continent updated successfully !');
+          this.router.navigate(['/']);
+        },
+        error: (err) => console.error('Error updating continent: ', err),
+      });
+    } else {
+      this.continentService.createContinent(this.continent).subscribe({
+        next: () => {
+          alert('Continent created successfully !');
+          this.router.navigate(['/']);
+        },
+        error: (err) => console.error('Error creating continent: ', err),
+      });
+    }
+  }
+
+  cancel(): void {
+    this.router.navigate(['/']);
+  }
 }
